f55d9e2030eb163b6ddb9576fbcae5acd6fe8a72
shivmohan <shivmohan@cloudzappy.com>
2026-09-10 13:28:34 +0530

    multiple body paramers

--- app/main_2.py
+++ app/main_2.py
@@ -8,16 +8,20 @@ class Product(BaseModel):
     id      : int
     name    : str
     price   : float
-    stock   : int|None = None
+    stock   : int | None = None
 
+
+class Seller(BaseModel):
+    username: str
+    full_name: str | None = None
 
 @app.post('/products')
-async def create_product(new_product:Product):
+async def create_product(new_product:Product, seller:Seller|None=None):
     product_dict = new_product.model_dump()
     product_with_tax = new_product.price + (new_product.price * 18 / 100)
     product_dict.update({"product_with_tax": product_with_tax})
     # return new_product
-    return product_dict
+    return {"product":product_dict , "seller":seller}       
 
 
 # add query parameter
